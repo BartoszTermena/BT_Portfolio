70469da55df3321e24476af0bf0656e6019c6b19
unknown <bartosz.termena@gmail.com>
2019-06-22 20:06:17 +0200

    Created new background

--- src/components/layout/Background.js
+++ src/components/layout/Background.js
@@ -2,7 +2,6 @@ import React from "react";
 import circle_medium from "./images/circle-medium.svg";
 import circle_medium_md from "./images/circle-medium-md.svg";
 import circle_large_md from "./images/circle-large-md.svg";
-import circle_large_sm from "./images/circle-large-sm.svg";
 import circle_large from "./images/circle-large.svg";
 
 const Background = () => {
@@ -10,7 +9,6 @@ const Background = () => {
     <div>
       <img src={circle_large} alt="circle" className="circle-large" />
       <img src={circle_large_md} alt="circle" className="circle-large-md" />
-      <img src={circle_large_sm} alt="circle" className="circle-large-sm" />
       <img src={circle_medium} alt="circle" className="circle-medium" />
       <img src={circle_medium_md} alt="circle" className="circle-medium-md" />
     </div>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,27 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import home from "./images/home.svg";
+import HamburgerMenu from "react-hamburger-menu";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(!open);
+  };
   return (
     <Fragment>
       <header>
         <div className="logo-container">
-          <img src={home} className="logo" alt="logo"/>
+        <HamburgerMenu
+					isOpen={open}
+					menuClicked={() => handleClick()}
+					width={25}
+					height={17}
+					strokeWidth={2}
+					rotate={0}
+          color='black'
+					borderRadius={0}
+					animationDuration={0.5}
+				/>
         </div>
-        <nav>
-          <ul className="nav-links">
-            <li>
-              <a className="nav-link" href="#">
-                My Work
-              </a>
-            </li>
-            <li>
-              <a className="nav-link" href="#">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
       </header>
     </Fragment>
   );
