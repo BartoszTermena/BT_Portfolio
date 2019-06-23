a5c4569805e2a3de6617c6f894a9b2f770bcba0a
unknown <bartosz.termena@gmail.com>
2019-06-23 15:44:46 +0200

    Working on portfolio

--- src/components/pages/MyWork.js
+++ src/components/pages/MyWork.js
@@ -1,65 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Background from "../layout/Background";
+import work_1 from "../layout/images/work1.png";
+import work_2 from "../layout/images/work2.png";
+import work_3 from "../layout/images/work3.png";
 const MyWork = () => {
-  return (
-    <React.Fragment>
-      <Background />
-
-      <div className="my-work container py-5">
-        <div class="card-group">
-          <div class="card">
-            <img
-              src="https://www.johnsmilga.com/static/184279132824c0bfe07d6fab388180f2/045aa/project-1.png"
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p class="card-text">
-                <small class="text-muted">Last updated 3 mins ago</small>
-              </p>
-            </div>
-          </div>
-          <div class="card">
-            <img
-              src="https://www.johnsmilga.com/static/184279132824c0bfe07d6fab388180f2/045aa/project-1.png"
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                This card has supporting text below as a natural lead-in to
-                additional content.
-              </p>
-              <p class="card-text">
-                <small class="text-muted">Last updated 3 mins ago</small>
-              </p>
-            </div>
-          </div>
-          <div class="card">
-            <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
-              </p>
-              <p class="card-text">
-                <small class="text-muted">Last updated 3 mins ago</small>
-              </p>
-            </div>
-          </div>
-        </div>
-        
+  const [pages, setPages] = useState([
+    {
+      link: "https://tshirt-project.firebaseapp.com/",
+      img: work_1,
+      title: "",
+      content: ""
+    },
+    {
+      link: "https://contact-keeper-b50a2.firebaseapp.com/",
+      img: work_2,
+      title: "",
+      content: ""
+    },
+    {
+        link: "https://vigilant-jennings-615463.netlify.com/",
+        img: work_3,
+        title: "",
+        content: ""
+      },
+  ]);
+  let list = pages.map(page => (
+    <div class="card">
+      <img src={page.img} class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+        <p class="card-text">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </p>
       </div>
-    </React.Fragment>
+    </div>
+  ));
+  return (
+    <div className="my-work container py-5">
+      <div class="card-group">{list}</div>
+    </div>
   );
 };
 
